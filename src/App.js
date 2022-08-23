8519cd1125112fa1b43c434d64495e6050aa70a0
swayamshreemohanty <mohantyswayamshree@gmail.com>
2022-08-23 10:40:19 +0530

    item repeatation added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ function App() {
     <div className="App">
       <h2>Let's get started</h2>
       <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }

026be2e4516fe0f320b4f9912d6e2f3018e41f3c
swayamshreemohanty <mohantyswayamshree@gmail.com>
2022-08-23 09:05:05 +0530

    Expense item component added

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>Date</div>
+      <div>
+        <h2>Expense item!</h2>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;

cafea4a57b00409d1b1eaa40e1211337498c06cd
swayamshreemohanty <mohantyswayamshree@gmail.com>
2022-08-23 10:40:07 +0530

    expenseitem css added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,14 @@
+import "./ExpenseItem.css";
 function ExpenseItem() {
+  const expenseDate = new Date(2022, 8, 23).toISOString();
+  const expenseTitle = "Car Insuranceas";
+  const expenseAmount = 294.67;
   return (
-    <div>
-      <div>Date</div>
+    <div className="expense-item">
+      <div className="expense-item__description">{expenseDate}</div>
       <div>
-        <h2>Expense item!</h2>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );

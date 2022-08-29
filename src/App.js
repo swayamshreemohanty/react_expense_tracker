b833d679b8113a1c2988bab3b72b7f4b3f627a46
swayamshreemohanty <mohantyswayamshree@gmail.com>
2022-08-29 10:44:58 +0530

    expenses component added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from "./components/Expenses";
 
+import "./components/Expenses.css";
 function App() {
+  const expenses = [
+    {
+      id: "1",
+      title: "Toilet paper",
+      amount: 94,
+      date: new Date(2022, 8, 29),
+    },
+    {
+      id: "2",
+      title: "Car insurance",
+      amount: 20,
+      date: new Date(2022, 6, 12),
+    },
+    {
+      id: "3",
+      title: "JIO recharge",
+      amount: 498,
+      date: new Date(2021, 5, 30),
+    },
+  ];
+
   return (
     <div className="App">
       <h2>Let's get started</h2>
-      <ExpenseItem></ExpenseItem>
-      <ExpenseItem></ExpenseItem>
-      <ExpenseItem></ExpenseItem>
-      <ExpenseItem></ExpenseItem>
-      <ExpenseItem></ExpenseItem>
+      <Expenses expensesList={expenses}></Expenses>
     </div>
   );
 }
